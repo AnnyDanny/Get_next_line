93328a7f780c930b92b585c1513746cf3a57bfef
Ganna DANYLOVA <gdanylov@e1r3p14.unit.ua>
2018-02-15 16:59:38 +0200

    withstructure

--- get_next_line.c
+++ get_next_line.c
@@ -9,7 +9,9 @@
 /*   Updated: 2018/01/13 21:07:03 by gdanylov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include <stdio.h>
+#include "libft.h"
 #include "get_next_line.h"
 #include <stdlib.h>
 #include <sys/types.h>
@@ -17,17 +19,11 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
-#include "libft.h"
 
-// void	free_all(char *strfree, t_list listfree)
-// {
-
-// }
-
-t_list	*multifile_get_in_list(int fd, t_list **vika)
+t_list			*multifile_get_in_list(int fd, t_list **vika)
 {
-	t_list *buf;
-	t_list *new;
+	t_list	*buf;
+	t_list	*new;
 
 	if (!vika)
 	{
@@ -37,116 +33,65 @@ t_list	*multifile_get_in_list(int fd, t_list **vika)
 	}
 	buf = *vika;
 	while (buf && (size_t)fd != buf->content_size)
- 	{
-		// printf("multifile buf with first call %s\n", buf->content);
- 		buf = buf->next;
- 	}
- 	if (buf)
- 	 	// printf("multifile buf second %s\n", buf->content);
- 	if (buf && buf->content_size == (size_t)fd)
- 	{
- 		// printf("multifile2 buf second %s\n", buf->content);
- 		return (buf);
- 	}
- 	new = ft_lstnew("", 1);
- 	// printf("pointer vika in multifile %p\n", vika);
- 	// printf("pointer new %p\n", new);
- 	new->content_size = fd;
- 	ft_lstadd(vika, new);
- 	return (*vika);
+		buf = buf->next;
+	if (buf && buf->content_size == (size_t)fd)
+		return (buf);
+	new = ft_lstnew("", 1);
+	new->content_size = fd;
+	ft_lstadd(vika, new);
+	return (*vika);
 }
 
-int get_in_list(char *tmp, t_list *list, char **line)
+int				get_in_list(char *tmp, t_list *list, char **line)
 {
-	int i;
-	char *tmp2;
-	char *g;
-	
+	int		i;
+	char	*tmp2;
+	char	*g;
+
 	i = 0;
-	// printf("tmp before strsub in line %s\n", tmp);
-	// printf("line before strsub from tmp %s\n", *line);
 	g = tmp;
 	*line = ft_strsub(tmp, 0, ft_countstrclen(tmp, '\n'));
-	// printf("line after strsub from tmp %s\n", *line);
-	// printf("tmp %s\n", tmp);
 	if ((tmp = ft_strchr(tmp, '\n')) != NULL)
 	{
-		// printf("tmp before in list %s\n", tmp);
 		tmp++;
-		// printf("tmp after tmp++ %s\n", tmp);
 		tmp2 = list->content;
 		list->content = ft_strdup(tmp);
 		free(tmp2);
-		// printf("list after strdub from tmp %s\n", list->content);
-		// printf("tmp after in list %s\n", tmp);
 	}
 	else
 		ft_bzero(list->content, ft_strlen(list->content));
-	// printf("pointer tmp in get in list %p\n", tmp);
-	// printf("pointer list %p\n", list);
-	// printf("pointer line %p\n", line);
 	free(g);
 	return (1);
 }
 
-int get_next_line(const int fd, char **line)
+int				get_next_line(const int fd, char **line)
 {
-	char *buf;
-	char *tmp;
-	int ret;
-	t_list *buf2;
-	static t_list *vika;
-	char *t;
-	char *d;
-	// t_list *vika2;
+	static		t_list *vika;
+	t_struct	s;
 
-	buf = ft_strnew(BUFF_SIZE);
- 	tmp = ft_strnew(BUFF_SIZE);
- 	buf2 = multifile_get_in_list(fd, &vika);
-	// printf("\nfd current %d\n", fd);
-	// printf("vika in gnl %s fd in vika %zu\n", vika->content, vika->content_size);
-	// vika2 = vika;
- // 	while (vika2)
- // 	{
- 		// printf("vika2 in gnl %s fd in vika %zu\n", vika2->content, vika2->content_size);
- 		// vika2 = vika2->next;
- 	// }
- 	// printf("buf2 %s fd in buf2 %zu\n", buf2->content, buf2->content_size);
- 	// tmp = buf2->content; does not matter here
- 	// printf("buf with first call %s\n", buf);
- 	// printf("tmp with first call %s\n", tmp);
-	while ((ret = read(fd, buf, BUFF_SIZE)) > 0)
+	s.buf = ft_strnew(BUFF_SIZE);
+	s.tmp = ft_strnew(BUFF_SIZE);
+	s.buf2 = multifile_get_in_list(fd, &vika);
+	while ((s.ret = read(fd, s.buf, BUFF_SIZE)) > 0)
 	{
-		// printf("buf before join in tmp %s\n", buf);
-		t = tmp;
-		tmp = ft_strjoin(tmp, buf);
-		free(t);
-		ft_strclr(buf);
-		// printf("tmp after join with buf %s\n", tmp);
-		// printf("buf after join to tmp %s\n", buf);
-		if (ft_strchr(tmp, '\n'))
+		s.t = s.tmp;
+		s.tmp = ft_strjoin(s.tmp, s.buf);
+		free(s.t);
+		ft_strclr(s.buf);
+		if (ft_strchr(s.tmp, '\n'))
 			break;
 	}
-	free(buf);
-	d = tmp;
-	tmp = ft_strjoin(buf2->content, tmp);
-	free(d);
-	// printf("tmp after while %s\n", tmp);
-	if (ft_strlen(tmp) == 0 && ret == 0 && (vika == NULL || ft_strlen(buf2->content) == 0))
+	free(s.buf);
+	s.d = s.tmp;
+	s.tmp = ft_strjoin(s.buf2->content, s.tmp);
+	free(s.d);
+	if (ft_strlen(s.tmp) == 0 && s.ret == 0 && (vika == NULL || ft_strlen(s.buf2->content) == 0))
 	{
-		free(tmp);
+		free(s.tmp);
 		return (0);
 	}
-	if (ret == -1)
+	if (s.ret == -1)
 		return (-1);
-	// if (vika)
-	// 	tmp = ft_strjoin(vika->content, tmp);
-	get_in_list(tmp, buf2, line);
-	// printf("pointer buf in gnl %p\n", buf);
-	// printf("pointer tmp in gnl %p\n", tmp);
-	// printf("pointer buf2 %p\n", buf2);
-	// printf("pointer vika in gnl %p\n", vika);
-	// system("leaks gnl");
+	get_in_list(s.tmp, s.buf2, line);
 	return (1);
 }
-
--- get_next_line.h
+++ get_next_line.h
@@ -10,10 +10,23 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 #ifndef GNL_H
 # define GNL_H
 
 # define BUFF_SIZE 5
+
+typedef struct	t_struct
+{
+	char		*t;
+	char		*d;
+	int			ret;
+	char		*buf;
+	char		*tmp;
+	t_list		*buf2;
+	// static		t_list *vika;
+}				t_struct;
 
 int 	get_next_line(const int fd, char **line);
 
